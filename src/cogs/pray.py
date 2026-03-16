9b0843fac727c6a822b1e6eed426951f8e7dc06a
DylanBurnsFont <dylan.in.celra@gmail.com>
2026-03-16 18:59:40 +0100

    Added new pray images and made it so response is randomized from one of them

--- src/cogs/pray.py
+++ src/cogs/pray.py
@@ -2,8 +2,11 @@ import discord
 from discord import app_commands
 from discord.ext import commands
 from pathlib import Path
+import random
+import os
 
-FORTUNE_GOD_PATH = Path(__file__).resolve().parents[2] / "FortuneGod.png"
+FORTUNE_GOD_PATH = Path(__file__).resolve().parents[2] / "PrayImages"
+images = os.listdir(FORTUNE_GOD_PATH)
 
 
 class PrayCog(commands.Cog):
@@ -13,9 +16,10 @@ class PrayCog(commands.Cog):
     @app_commands.command(name="pray", description="Offer a prayer to the Fortune God")
     async def pray_command(self, interaction: discord.Interaction):
         username = interaction.user.display_name
+        randomIndex = random.randint(0, len(images) - 1)
         await interaction.response.send_message(
-            f"Your prayer has been heard **{username}** 🙏",
-            file=discord.File(FORTUNE_GOD_PATH),
+            f"🙏 Your prayer has been heard **{username}** 🙏",
+            file=discord.File(FORTUNE_GOD_PATH / images[randomIndex]),
         )
 
 
